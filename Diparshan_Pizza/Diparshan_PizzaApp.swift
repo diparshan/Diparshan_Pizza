d27eb4a5565ebc48be905c82a6fe11317eb606bd
Diparshan <diparshan10@gmail.com>
2024-02-02 15:02:18 -0500

    Initial Commit

---
+++ Diparshan_Pizza/Diparshan_PizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Diparshan_PizzaApp.swift
+//  Diparshan_Pizza
+//
+//  Created by Diparshan Bhattarai on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Diparshan_PizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
